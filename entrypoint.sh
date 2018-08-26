45ec8279265a69e688cbc44bd8cdc85d1ff61471
giegloop <42495472+giegloop@users.noreply.github.com>
2018-08-26 13:04:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ wget -O goflyway.tar.gz https://github.com/coyove/goflyway/releases/download/v1.
 tar zxf goflyway.tar.gz
 cd /goflyway/goflyway_linux_amd64
 chmod +x goflyway 
- ./goflyway -k=30522b10-a8d2-11e8-978c-f0d5bf44df99  
+ ./goflyway -k=30522b10-a8d2-11e8-978c-f0d5bf44df99  -l=":443"
